d31f640804e40ea12e3e1c55bb233cd61a3f5739
Shaker Mahmud Khandaker <shaker@khandakerrahin.com>
2022-05-18 17:54:47 +0200

    Transfer from Bitbucket to Github

---
+++ rfid_services/ShakerRahin-sdcdl950s_console-97d0af4471af/ShakerRahin-sdcdl950s_console-97d0af4471af/DL950_console/LogSwitchTimer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DL950_console
+{
+    class LogSwitchTimer
+    {
+        int logSwitchTimerInMillis;
+        public LogSwitchTimer()
+        {
+            this.logSwitchTimerInMillis = 60000; //60000
+        }
+        public LogSwitchTimer(int logSwitchTimerInMinutes)
+        {
+            this.logSwitchTimerInMillis = logSwitchTimerInMinutes * 60000;
+        }
+        public void checkTimeForLogSwitch()
+        {
+            while (Program.isRunning)
+            {
+                try
+                {
+                    //Console.WriteLine("LogSwitchTimer is running on another thread.");
+                    Thread.Sleep(logSwitchTimerInMillis);
+                    if (Program.isRunning)
+                    {
+                        String lastLogFilename = Program.filename;
+                        Program.isSwitchingLog = true;
+                        Program.switchLog();
+                        Program.isSwitchingLog = false;
+                        LogSwitchTimer.removeLastEmptyLogFile(lastLogFilename); //can be done in a separate thread
+                        lastLogFilename = Program.applicationLogFilename;
+                        Program.isSwitchingApplicationLog = true;
+                        Program.switchApplicationLog();
+                        Program.isSwitchingApplicationLog = false;
+                        LogSwitchTimer.removeLastEmptyLogFile(lastLogFilename); //can be done in a separate thread
+                    }
+                    //Console.WriteLine("The checkTimeForLogSwitch method called by the worker thread has ended.");
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.ToString());
+                }
+            }
+        }
+
+        internal static void removeLastEmptyLogFile(string lastLogFilename)
+        {
+            FileInfo fileInfo = new FileInfo(lastLogFilename);
+            if (fileInfo.Exists)
+            {
+                if (fileInfo.Length == 0 || fileInfo.Length < 2)
+                {
+                    //empty file. Delete it.
+                    try
+                    {
+                        File.Delete(lastLogFilename);
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.WriteLine(e.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
